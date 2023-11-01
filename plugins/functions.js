d6a5cb35256a3131f68c4f4616612bc85b75d849
Joseph K <double.facessss@gmail.com>
2023-11-01 18:25:58 +0100

    added plugin functions.js

---
+++ plugins/functions.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+
+Vue.mixin({
+    methods: {
+        createPayload: function(key, value) {
+            return { 
+                key,
+                value
+            }
+        },
+        detectViewport: function() {            
+            const viewportWidth = window.innerWidth
+            let payload            
+            const mobileBreakpoint = 768
+            if(viewportWidth < mobileBreakpoint) {                
+                console.log('window: viewport is mobile')
+                payload = this.createPayload('isMobile', true);
+                this.$store.commit('appStore/commitAppState', payload)
+                //return true
+            } else {                
+                console.log('window: viewport is desktop')
+                payload = this.createPayload('isMobile', false);
+                this.$store.commit('appStore/commitAppState', payload)
+                //return false
+            }
+        }
+    }
+})
