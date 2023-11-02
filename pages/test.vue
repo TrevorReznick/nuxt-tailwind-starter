b1cb41c174b5dbce31024b77bf094eade6b14563
Joseph K <double.facessss@gmail.com>
2023-11-02 17:44:47 +0100

    set and test dark mode

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,71 @@
 <template>
-    <div>
+    <div class="bg-white dark:bg-gray-800">
+        <button 
+            id="theme-toggle" 
+            type="button" 
+            class="
+                text-gray-500 
+                dark:text-gray-400 
+                hover:bg-gray-100 
+                dark:hover:bg-gray-700 
+                focus:outline-none 
+                focus:ring-4 
+                focus:ring-gray-200 
+                dark:focus:ring-gray-700 
+                rounded-lg 
+                text-sm p-2.5
+            "
+        >
+            <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+            <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+        </button>
+
+        
         <div class="flex justify-center p-4">
-            <button id="button" data-modal-toggle="modal" data-modal-target="modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Show modal</button>
+            <button 
+                id="button" 
+                data-modal-toggle="modal" 
+                data-modal-target="modal" 
+                type="button" 
+                class="
+                    text-white 
+                    bg-blue-700 
+                    hover:bg-blue-800 
+                    focus:ring-4 
+                    focus:ring-blue-300 
+                    font-medium 
+                    rounded-lg 
+                    text-sm px-5 
+                    py-2.5 
+                    mr-2 
+                    mb-2 
+                    dark:bg-blue-600 
+                    dark:hover:bg-blue-700 
+                    focus:outline-none 
+                    dark:focus:ring-blue-800
+                "
+            >Show modal</button>
         </div>
 
-        <div id="modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div 
+            id="modal" 
+            tabindex="-1" 
+            aria-hidden="true" 
+            class="
+                fixed 
+                top-0 
+                left-0 
+                right-0 
+                z-50 
+                hidden 
+                w-full 
+                p-4 overflow-x-hidden 
+                overflow-y-auto 
+                md:inset-0 
+                h-[calc(100%-1rem)] 
+                max-h-full
+                "
+            >
             <div class="relative w-full max-w-2xl max-h-full">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -43,10 +104,15 @@ import { initFlowbite } from 'flowbite'
 
 export default {
   
-  mounted: function() {
-    initFlowbite()
-  }
+    mounted: function() {
+        initFlowbite()
+        /* @@ check dark mode selection @@ */
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+        this.setDarkMode()
+    }
 }
-
 </script>
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,6 @@ module.exports = {
   },
   plugins: [
     require('flowbite')
-  ]
+  ],
+  darkMode: 'class'
 }
